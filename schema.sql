44433e4739314da483bddd3c750e7fe85fe9834a
zeil12 <braska321@gmail.com>
2019-11-05 14:36:27 +0300

    module4-task1

--- schema.sql
+++ schema.sql
@@ -4,39 +4,40 @@ DEFAULT COLLATE utf8_general_ci;
 
 USE doingsdone;
 
+CREATE TABLE user (
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    registration_date   TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    email      VARCHAR(30) UNIQUE NOT NULL,
+    user_name  VARCHAR(30) NOT NULL,
+    password   VARCHAR(40) NOT NULL,
+    INDEX (registration_date),
+    UNIQUE UK_email (email),
+    INDEX (user_name)
+);
+
 CREATE TABLE project (
     id         INT AUTO_INCREMENT PRIMARY KEY,
     title      VARCHAR(50) NOT NULL,
     user_id    INT NOT NULL,
-    FOREIGN KEY (user_id)  REFERENCES user (id)
+    FOREIGN KEY (user_id)  REFERENCES user (id),
+    INDEX (user_id)
 );
-
-CREATE INDEX pj_name ON project(title);
 
 CREATE TABLE task (
     id         INT AUTO_INCREMENT PRIMARY KEY,
-    crn_date   TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    creation_date   TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
     status     INT NOT NULL,
-    tsk_name   VARCHAR(50) NOT NULL,
+    task_name   VARCHAR(50) NOT NULL,
     file_link  VARCHAR(100),
-    deadline   TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
+    deadline   DATETIME NOT NULL,
     user_id    INT NOT NULL,
     project_id    INT NOT NULL,
     FOREIGN KEY (user_id)     REFERENCES user (id),
-    FOREIGN KEY (project_id)  REFERENCES project (id)
+    FOREIGN KEY (project_id)  REFERENCES project (id),
+    INDEX (creation_date)
 );
 
-CREATE INDEX cr_date ON task(crn_date);
 
-CREATE TABLE user (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    reg_date   TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
-    email      VARCHAR(30) UNIQUE NOT NULL,
-    user_name  VARCHAR(30) NOT NULL,
-    password   VARCHAR(40) NOT NULL
-);
 
-CREATE INDEX r_d ON user(reg_date);
-CREATE UNIQUE INDEX mail ON user(email);
-CREATE INDEX u_name ON user(user_name);
+
 
